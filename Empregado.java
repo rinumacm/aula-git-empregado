efe00dd7beb3fb7ccdddac7517bdea6cdf842da8
Munir Camacho <rinumps4@gmail.com>
2023-03-27 20:57:38 -0300

    incluindo arquivos faltantes do commit anterior

--- Empregado.java
+++ Empregado.java
@@ -34,4 +34,13 @@ public class Empregado {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int tipo() {
+        return this.tipo;
+    }
+
 }
