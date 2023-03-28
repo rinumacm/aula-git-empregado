ec3fc248be52561d0e15675c0919fd7d9c7f41d6
Munir Camacho <rinumps4@gmail.com>
2023-03-27 21:03:54 -0300

    Primeiro teste concluido com sucesso

--- Empregado.java
+++ Empregado.java
@@ -43,4 +43,28 @@ public class Empregado {
         return this.tipo;
     }
 
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getComissao() {
+        return this.comissao;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return this.bonus;
+    }
+
 }
